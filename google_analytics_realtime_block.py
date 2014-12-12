727d479db87ed08a26495e43c82c6f0ed998d504
Matt Dodge <matt@neutral.io>
2014-12-11 16:34:16 -0800

    BLK-197 Adding realtime analytics block

---
+++ google_analytics_realtime_block.py
@@ -0,0 +1,36 @@
+from nio.common.discovery import Discoverable, DiscoverableType
+from nio.metadata.properties import ListProperty
+from .google_oauth_base.google_oauth_block import GoogleOAuth
+
+
+@Discoverable(DiscoverableType.block)
+class GoogleAnalyticsRealtime(GoogleOAuth):
+
+    analytics_ids = ListProperty(
+        str, title="Analytics IDs", default=["ga:########"])
+    metrics = ListProperty(
+        str, title="Analytics Metrics", default=["rt:activeUsers"])
+    dimensions = ListProperty(
+        str, title="Analytics Dimensions", default=["rt:city"])
+
+    def get_google_scope(self):
+        """ Required override for GoogleOAuth Block """
+        return 'https://www.googleapis.com/auth/analytics.readonly'
+
+    def get_url_suffix(self):
+        """ Required override for GoogleOAuth Block """
+        return 'analytics/v3/data/realtime'
+
+    def get_url_parameters(self):
+        """ Required override for GoogleOAuth Block """
+        params = {
+            "ids": ",".join(self.analytics_ids),
+            "metrics": ",".join(self.metrics),
+            "dimensions": ",".join(self.dimensions)
+        }
+
+        # Include any additional parameters from the parent block
+        params.update(self.get_addl_params())
+
+        self._logger.debug("Accessing Analytics API using {0}".format(params))
+        return params
