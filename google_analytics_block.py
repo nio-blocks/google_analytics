c3687bd145bce5d1d02af9174336c871aae34b00
Matt Dodge <matt@neutral.io>
2014-12-15 14:53:29 -0800

    BLK-200 Refactoring analytics blocks to use new REST polling Allows for multiple analytics IDs now

--- google_analytics_block.py
+++ google_analytics_block.py
@@ -1,37 +1,21 @@
 from nio.common.discovery import Discoverable, DiscoverableType
-from nio.metadata.properties import ListProperty, IntProperty
-from .google_oauth_base.google_oauth_block import GoogleOAuth
+from nio.metadata.properties import IntProperty
+from .google_analytics_base import GoogleAnalyticsBase
 
 
 @Discoverable(DiscoverableType.block)
-class GoogleAnalytics(GoogleOAuth):
+class GoogleAnalytics(GoogleAnalyticsBase):
 
-    analytics_ids = ListProperty(
-        str, title="Analytics IDs", default=["ga:########"])
-    metrics = ListProperty(
-        str, title="Analytics Metrics", default=["ga:hits"])
     lookback_days = IntProperty(title="Lookback Days (inclusive)", default=0)
 
-    def get_google_scope(self):
-        """ Required override for GoogleOAuth Block """
-        return 'https://www.googleapis.com/auth/analytics.readonly'
-
     def get_url_suffix(self):
-        """ Required override for GoogleOAuth Block """
         return 'analytics/v3/data/ga'
 
     def get_url_parameters(self):
-        """ Required override for GoogleOAuth Block """
-        params = {
-            "ids": ",".join(self.analytics_ids),
-            "start-date": "{0}daysAgo".format(self.lookback_days),
-            "end-date": "today",
-            "metrics": ",".join(self.metrics),
-            "dimensions": "ga:userType"
-        }
+        params = super().get_url_parameters()
 
-        # Include any additional parameters from the parent block
-        params.update(self.get_addl_params())
+        params["start-date"] = "{0}daysAgo".format(self.lookback_days)
+        params["end-date"] = "today"
+        params["dimensions"] = "ga:userType"
 
-        self._logger.debug("Accessing Analytics API using {0}".format(params))
         return params
